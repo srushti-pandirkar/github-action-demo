80e01533e38c603db2024420fc71e4351f269d7b
Srushti Pandirkar <srushti.pandirkar@atos.net>
2023-04-26 14:42:32 +0530

    initial commit

---
+++ src/main/java/com/crud/springsampleapp/HomeController.java
@@ -0,0 +1,13 @@
+package com.crud.springsampleapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String home(){
+        return "Hello From Spring Boot...";
+    }
+}
---
+++ src/test/java/com/crud/springsampleapp/SpringSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.springsampleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
